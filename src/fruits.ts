d497d8d7da0c351c2e1178150d0cf0202c6d227c
WhiteKr <hanjisu2215g@gmail.com>
2024-02-17 22:29:41 +0900

    feat: add fruits data and addFruit

---
+++ src/fruits.ts
@@ -0,0 +1,63 @@
+export type Fruit = {
+  name: string;
+  color: string;
+  radius: number;
+};
+
+export const FRUITS: Fruit[] = [
+  {
+    name: "cherry",
+    color: "red",
+    radius: 33 / 2,
+  },
+  {
+    name: "strawberry",
+    color: "orangered",
+    radius: 48 / 2,
+  },
+  {
+    name: "grape",
+    color: "purple",
+    radius: 61 / 2,
+  },
+  {
+    name: "mandarin",
+    color: "orange",
+    radius: 69 / 2,
+  },
+  {
+    name: "orange",
+    color: "darkorange",
+    radius: 89 / 2,
+  },
+  {
+    name: "apple",
+    color: "red",
+    radius: 114 / 2,
+  },
+  {
+    name: "pear",
+    color: "yellow",
+    radius: 129 / 2,
+  },
+  {
+    name: "peach",
+    color: "pink",
+    radius: 156 / 2,
+  },
+  {
+    name: "pineapple",
+    color: "yellow",
+    radius: 177 / 2,
+  },
+  {
+    name: "melon",
+    color: "lightgreen",
+    radius: 220 / 2,
+  },
+  {
+    name: "watermelon",
+    color: "green",
+    radius: 259 / 2,
+  },
+];
--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
 import { Bodies, Engine, Render, Runner, World } from "matter-js";
+import { FRUITS, Fruit } from "./fruits";
 
 // setup world
 const [worldHeight, worldWidth]: number[] = [850, 620];
@@ -23,15 +24,16 @@ const wallWidth: number = 10;
 const groundWidth: number = 20;
 const topLinePosition: number = 130;
 
-const wallPositions: [number, number, number, number][] = [
+const wallPositions: [number, number, number, number, boolean?][] = [
   [wallWidth / 2, worldHeight / 2, wallWidth, worldHeight], // left wall
   [worldWidth - wallWidth / 2, worldHeight / 2, wallWidth, worldHeight], // right wall
   [worldWidth / 2, worldHeight - groundWidth / 2, worldWidth, groundWidth], // ground
-  [worldWidth / 2, topLinePosition, worldWidth, 2], // top line
+  [worldWidth / 2, topLinePosition, worldWidth, 2, true], // top line
 ];
-const walls: Matter.Body[] = wallPositions.map((pos: number[]) =>
+const walls: Matter.Body[] = wallPositions.map((pos) =>
   Bodies.rectangle(pos[0], pos[1], pos[2], pos[3], {
     isStatic: true,
+    isSensor: pos[4] ?? false,
     render: { fillStyle: wallBackgroundColor },
   }),
 );
@@ -40,3 +42,21 @@ World.add(world, walls);
 
 Render.run(render);
 Runner.run(engine);
+
+const addFruit = () => {
+  const index: number = (Math.random() * 5) | 0;
+  const fruit: Fruit = FRUITS[index];
+
+  const body = Bodies.circle(300, 50, fruit.radius, {
+    // index,
+    label: fruit.name,
+    restitution: 0.2,
+    render: {
+      fillStyle: fruit.color,
+    },
+  });
+
+  World.add(world, body);
+};
+
+addFruit();
